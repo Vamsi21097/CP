67ea47a819270bf31cd0a8fdc8f262ddb4b54596
vamsi21097 <vamsi21097@gmail.com>
2021-08-24 21:48:01 +0530

    hasconsecutive

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -4,4 +4,11 @@
 
 def hasconsecutivedigits(n):
 	# your code goes here
-	pass
+	n = str(n)
+	if len(n)==1:
+		return False
+	for i in range(len(n)-1):
+		if(n[i] == n[i+1]):
+			return True
+	return False
+			
