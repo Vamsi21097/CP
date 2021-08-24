b002999f938fa9b1e48e64fa56c73d15aeeec56d
vamsi21097 <vamsi21097@gmail.com>
2021-08-24 22:02:47 +0530

    stacks

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -10,11 +10,9 @@ class Element(object):
     def __init__(self, value):
         self.value = value
         self.next = None
-        
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
-        
     def append(self, new_element):
         current = self.head
         if self.head:
@@ -23,24 +21,40 @@ class LinkedList(object):
             current.next = new_element
         else:
             self.head = new_element
-
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
-
+        if self.head is None:
+             self.head = Element(new_element)
+        else:
+            newnode = Element(new_element)
+            newnode.next = self.head
+            self.head = newnode
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        if self.head:
+            previous = self.head
+            if previous.next:   
+                deleted = previous.value         
+                self.head = previous.next
+            else:
+                value = previous.value
+                deleted = Element(value)
+                self.head.value = None
+            return deleted
+        else:
+            return None
 
 class stack(object):
     def __init__(self,top=None):
         self.ll = LinkedList(top)
-
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        pass
-
+        self.ll.insert_first(new_element)
     def pop(self):
-        "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        "Pop (remove) the first element off the top of the stack and return it"        
+        deleted = self.ll.delete_first()
+        if(deleted.value is None):
+            return None
+        else:
+            return deleted
     
