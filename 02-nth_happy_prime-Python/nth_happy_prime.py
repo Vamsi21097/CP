387354096f1bf4a277f37500352eda1664fb1749
vamsi21097 <vamsi21097@gmail.com>
2021-08-25 15:16:53 +0530

    nthhappyprime

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,44 @@
 
 
 
+def prime(n):
+ 	if(n<=1):
+ 		return False
+ 	if(n==2):
+ 		return True
+ 	if(n%2==0):
+ 		return False
+ 	for j in range(3,n):
+ 		if(n%j==0):
+ 			return False
+ 	return True
+
+
+def happy(m):
+ 	sum1=0
+ 	while(m>0):
+ 		n=m%10
+ 		sum1+=(n**2)
+ 		m=m//10
+ 	return sum1
+
+
 def fun_nth_happy_prime(n):
-	return 0
+ 	# return 0 
+ 	i=1
+ 	res=[]
+ 	while(len(res)<=n):
+ 		if(prime(i)):
+ 			s=i
+ 			for j in range(30):
+ 				p=happy(s)
+ 				# print(p,'p')
+ 				if(p==1):
+ 					res.append(i)
+ 					break
+ 				# if('4' in str(p)):
+ 				# 	break
+ 				s=p
+ 		i=i+1
+ 	print(res)
+ 	return res[n]
