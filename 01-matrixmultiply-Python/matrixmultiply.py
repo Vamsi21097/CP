82d3c8aef40c9e68f4708cc3c9118247a2780c99
vamsi21097 <vamsi21097@gmail.com>
2021-08-25 14:39:17 +0530

    matrixmultiply

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -3,9 +3,17 @@
 # is the result of multiplying the two matrices. Return None if the 
 # two matrices cannot be multiplied for any reason.
 
-
+import numpy as np
 def fun_matrixmultiply(m1, m2):
-    return None
+    if (len(m1[0]))!=len(m2):
+        return None
+
+    a = np.array(m1)
+    b = np.array(m2)
+    c = a.dot(b)
+    return c.tolist()
+
+print(fun_matrixmultiply([[1,3],[2,4],[2,5]], [[1,3,2,2], [2,4,5,1]]))
 
 
 
