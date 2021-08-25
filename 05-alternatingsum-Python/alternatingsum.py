bc3ecf6a12abaea85732d30f171c10fc4a602a5a
vamsi21097 <vamsi21097@gmail.com>
2021-08-25 14:28:12 +0530

    alternatingsum

--- 05-alternatingsum-Python/alternatingsum.py
+++ 05-alternatingsum-Python/alternatingsum.py
@@ -3,9 +3,14 @@
 # For example, alternatingSum([5,3,8,4]) returns 6 (that is, 5-3+8-4). If the list is empty, return 0.
 
 
-
-
 def fun_alternatingsum(a): 
-	return 0
+	b = 0
+	for i in range(0,len(a)):
+		if i%2==0:
+			b+=a[i]
+		elif i%2!=0:
+			b = b-a[i]
+	return b
+
 
 
