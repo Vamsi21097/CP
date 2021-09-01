65345769427c94755dbb20a5cfd25717b7d02139
vamsi21097 <vamsi21097@gmail.com>
2021-09-01 22:55:09 +0530

    wordwrap

--- 02-wordwrap-Python/wordwrap.py
+++ 02-wordwrap-Python/wordwrap.py
@@ -16,8 +16,17 @@
 # -fgh""")
 
 
-def fun_wordwrap(s, n):
-	return ""
+import os,sys
+sys.path.append(os.getcwd())
+from wordwrap import fun_wordwrap
+import pytest
 
+
+@pytest.mark.parametrize('s1, s2, result',[	
+(" a b c de fgh ",  4, "a-b-\nc-de\n-fgh"),
+ ("  abcdefghij", 4, "abcd\nefgh\nij")
+])
+def test_fun_wordwrap(s1, s2, result):
+    assert fun_wordwrap(s1, s2) == result
 
  
